391cf3957c3e35d3c10b2e3d74183ebc18070fc9
mghako <htetaungko@yahoo.com>
2021-11-29 17:20:50 +0630

    fixed cor setup

--- app/Http/Resources/Item/ItemResource.php
+++ app/Http/Resources/Item/ItemResource.php
@@ -15,6 +15,7 @@ class ItemResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'price' => $this->price,
             'size' => $this->size,
